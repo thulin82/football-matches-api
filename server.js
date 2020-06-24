d971bd418ef636bc581822e4845a57f28eb29c85
Markus Thulin <macky_b@hotmail.com>
2020-06-24 10:57:02 +0200

    Added middleware

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 const matches = require('./routes/matches');
 
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/matches', matches);
 
