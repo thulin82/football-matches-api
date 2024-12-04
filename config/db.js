575da5c314085bcc82992897d79900c51654018b
Markus Thulin <macky_b@hotmail.com>
2024-12-04 14:31:21 +0100

    Updated dependencies (#19)

--- config/db.js
+++ config/db.js
@@ -1,15 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
-    });
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
 
-    if (process.env.NODE_ENV === 'development') {
-        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+    if (process.env.NODE_ENV === "development") {
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
 };
 
--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-// eslint-disable-next-line no-unused-vars
-const colors = require('colors');
-const cors = require('cors');
-const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 connectDB();
 
-const matches = require('./routes/matches');
+const matches = require("./routes/matches");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
 }
 
-app.use('/api/v1/matches', matches);
+app.use("/api/v1/matches", matches);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(
-    PORT,
-    //console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
-);
+app.listen(PORT);
 
 module.exports = app;
