f8500ea99eccda93120c3095dce28b1a641316e7
Markus Thulin <macky_b@hotmail.com>
2024-12-04 20:47:28 +0100

    Rewrote tests (#22)

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,26 +1,33 @@
-import { describe, it, after } from "mocha";
-import chai from "chai";
-import server from "./utils/server.mock.js";
-const { should, expect } = chai;
-should();
+import { use } from "chai";
+import chaiHttp from "chai-http";
+import app from "../server.js";
+
+const chai = use(chaiHttp);
+const expect = chai.expect;
 
 describe("Routes", () => {
-    after(() => server.close());
-
-    it("GET /api/v1/matches should return json", (done) => {
-        server.get("/api/v1/matches").end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res.type).to.eql("application/json");
-            done();
+    describe("GET /api/v1/matches", () => {
+        it("should return json", (done) => {
+            chai.request
+                .execute(app)
+                .get("/api/v1/matches")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.type).to.eql("application/json");
+                    done();
+                });
         });
     });
-    it("GET /api/v1/matches/:id should return json", (done) => {
-        server
-            .get("/api/v1/matches/5ef355c7a7915e583038e9fc")
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.type).to.eql("application/json");
-                done();
-            });
+    describe("GET /api/v1/matches/:id", () => {
+        it("should return json", (done) => {
+            chai.request
+                .execute(app)
+                .get("/api/v1/matches/5ef355c7a7915e583038e9fc")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.type).to.eql("application/json");
+                    done();
+                });
+        });
     });
 });
