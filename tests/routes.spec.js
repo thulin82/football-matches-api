d4e3a77df88b5c780cea6d3b2249f7eb6fd10dee
Markus Thulin <macky_b@hotmail.com>
2024-12-04 20:46:44 +0100

    Finally?

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,15 +1,15 @@
-import chai from "chai";
+import { use } from "chai";
 import chaiHttp from "chai-http";
 import app from "../server.js";
 
-const { expect } = chai;
-
-chai.use(chaiHttp);
+const chai = use(chaiHttp);
+const expect = chai.expect;
 
 describe("Routes", () => {
     describe("GET /api/v1/matches", () => {
         it("should return json", (done) => {
-            chai.request(app)
+            chai.request
+                .execute(app)
                 .get("/api/v1/matches")
                 .end((err, res) => {
                     expect(res).to.have.status(200);
@@ -20,7 +20,8 @@ describe("Routes", () => {
     });
     describe("GET /api/v1/matches/:id", () => {
         it("should return json", (done) => {
-            chai.request(app)
+            chai.request
+                .execute(app)
                 .get("/api/v1/matches/5ef355c7a7915e583038e9fc")
                 .end((err, res) => {
                     expect(res).to.have.status(200);
