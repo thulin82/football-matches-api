2f572f3deefdcc9eaef87e076057560143d81f1e
Markus Thulin <macky_b@hotmail.com>
2024-12-04 20:25:43 +0100

    Rewrote tests

--- tests/routes.spec.js
+++ tests/routes.spec.js
@@ -1,26 +1,32 @@
-import { describe, it, after } from "mocha";
 import chai from "chai";
-import server from "./utils/server.mock.js";
-const { should, expect } = chai;
-should();
+import chaiHttp from "chai-http";
+import app from "../server.js";
+
+const { expect } = chai;
+
+chai.use(chaiHttp);
 
 describe("Routes", () => {
-    after(() => server.close());
-
-    it("GET /api/v1/matches should return json", (done) => {
-        server.get("/api/v1/matches").end((err, res) => {
-            expect(res).to.have.status(200);
-            expect(res.type).to.eql("application/json");
-            done();
+    describe("GET /api/v1/matches", () => {
+        it("should return json", (done) => {
+            chai.request(app)
+                .get("/api/v1/matches")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.type).to.eql("application/json");
+                    done();
+                });
         });
     });
-    it("GET /api/v1/matches/:id should return json", (done) => {
-        server
-            .get("/api/v1/matches/5ef355c7a7915e583038e9fc")
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                expect(res.type).to.eql("application/json");
-                done();
-            });
+    describe("GET /api/v1/matches/:id", () => {
+        it("should return json", (done) => {
+            chai.request(app)
+                .get("/api/v1/matches/5ef355c7a7915e583038e9fc")
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res.type).to.eql("application/json");
+                    done();
+                });
+        });
     });
 });
--- tests/utils/server.mock.js
+++
@@ -1,6 +0,0 @@
-import chai from "chai";
-import chaiHttp from "chai-http";
-import app from "../../server.js";
-
-chai.use(chaiHttp);
-export default chai.request(app).keepOpen();
